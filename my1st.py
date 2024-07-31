3ea50ce3a4aa5ed3ff21551d96c30f7ef4340546
ShahidulSaad <kmsaad01711@gmail.com>
2024-07-31 13:36:29 +0800

    1.2

--- my1st.py
+++ my1st.py
@@ -1,3 +1,17 @@
-import streamlit as st
+# import streamlit as st
 
-st.write("Hello world") 
+# st.write("Hello world") 
+
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
