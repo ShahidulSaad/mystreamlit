919064a7524adb6016b0748e99955a8c54141672
ShahidulSaad <kmsaad01711@gmail.com>
2024-07-31 15:04:38 +0800

    1.3

--- my1st.py
+++ my1st.py
@@ -1,7 +1,25 @@
-# import streamlit as st
+# 1st
+import streamlit as st
 
-# st.write("Hello world") 
+st.write("Hello world") 
 
+
+# 2nd 
+# My first app
+# Here's our first attempt at using data to create a table:
+
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
+
+# 3rd
 import streamlit as st
 import pandas as pd
 
@@ -15,3 +33,75 @@ option = st.selectbox(
      df['first column'])
 
 'You selected: ', option
+
+
+# 4th
+import streamlit as st
+import numpy as np
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
+
+
+
+# 5th
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+
+
+# 6th
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
+
+
+# 7th
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
+
+
+# 8th
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+
+# 9th
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+
+# 10th
+
